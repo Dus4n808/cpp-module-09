3d5b2d98e16b6493d2108cdb001903203482f247
Dusan <dusanfama@gmail.com>
2026-05-31 15:01:31 +0200

    start

---
+++ ex00/headers/BitcoinExchange.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dufama <dufama@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/31 14:40:43 by dufama            #+#    #+#             */
+/*   Updated: 2026/05/31 14:40:43 by dufama           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <map>
+#include <string>
+
+
+class BitCoinExchange {
+	private:
+		std::map<std::string, float> _data;
+	public:
+		// ===== FCO =====
+		
+};
+
+#endif
